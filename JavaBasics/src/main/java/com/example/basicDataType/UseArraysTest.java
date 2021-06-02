8335adaab1ce7e0fc9922f3e1c3875231a89f94f
xxmy <1471441495@qq.com>
2021-06-02 14:55:15 +0800

    style: 同步gitee

--- JavaBasics/src/main/java/com/example/basicDataType/UseArraysTest.java
+++ JavaBasics/src/main/java/com/example/basicDataType/UseArraysTest.java
@@ -27,5 +27,6 @@ public class UseArraysTest {
         int[][] magicSquare = {{16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12}, {4, 15, 14, 1}};
         System.out.println(Arrays.deepToString(magicSquare));
 
+
     }
 }
